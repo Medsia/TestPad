4de803f8bf2952ea9c7e0327d7e76f9dc87fbf2d
Dmitry Guznenok <medsia@mail.ru>
2023-01-30 21:01:16 +0300

    Add StartTest page

---
+++ LX.TestPad/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LX.TestPad.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("StartTest")]
+        public IActionResult StartTest()
+        {
+            return View();
+        }
+    }
+}

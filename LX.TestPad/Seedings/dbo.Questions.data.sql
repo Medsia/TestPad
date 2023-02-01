57dbb9572b24486698266ef83dc2f801cdd34331
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2023-02-01 06:26:03 +0300

    Seed update
    Created "Seedings" folder
    Create .sql scripts

---
+++ LX.TestPad/Seedings/dbo.Questions.data.sql
@@ -0,0 +1,18 @@
+﻿SET IDENTITY_INSERT [dbo].[Questions] ON
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES ( 1, N'Given Interface: Foo extends Behaviour . Can Foo extend additional interfaces?' )
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (2, N'Select keywords in Java')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (3, N'What would display from the following statements? int[]nums; {1,2,3,4,5,6}; Systom.out.println(( nums[1] + nums[3])); ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (4, N'Which SQL keywords is used to sort the result-set ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (5, N'Given: class Foo extends Bar ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (6, N'Which of the dollowing store elements as key-value pairs ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (7, N'Which SQL statement is used to extract data from a database? ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (8, N'What is a method signature ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (9, N'Given the declaration : int[] ar ={1,2,3,4,5}; What is the value of ar[3]? ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (10, N'A class is... ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (11, N'What will be the out for the following programm int a=10; System.out.println(+aa=a++); System.out.printl(a++=a++); ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (12, N'If none of the private/protected/public is specified for a member, that member... ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (13, N'The last value in an array callsed ar can be found at index... ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (14, N'Which one of the following statement is correct? ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (15, N'What will be the out for the following program: ')
+INSERT INTO [dbo].[Question] ([Id], [Text]) VALUES (16, N'Select statements which are correct about HTTP methods ')
+SET IDENTITY_INSERT [dbo].[Questions] OFF

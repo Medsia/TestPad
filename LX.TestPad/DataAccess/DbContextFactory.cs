0ea440f7b73609e189d61056a9b966d67c7a6aad
Dmitry Guznenok <medsia@mail.ru>
2023-02-01 14:46:53 +0300

    Create DbContextFactory

---
+++ LX.TestPad/DataAccess/DbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace LX.TestPad.DataAccess
+{
+    public class DbContextFactory
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public DbContextFactory(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public DataContext Create(Type repositoryType)
+        {
+            var services = httpContextAccessor.HttpContext.RequestServices;
+
+            var dbContexts = services.GetService<Dictionary<Type, DataContext>>();
+            if (!dbContexts.ContainsKey(repositoryType))
+                dbContexts[repositoryType] = services.GetService<DataContext>();
+
+            return dbContexts[repositoryType];
+        }
+    }
+}

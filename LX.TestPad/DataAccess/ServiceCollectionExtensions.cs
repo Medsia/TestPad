013f1d4d2eee304bbb90c09955553e0efd0521e0
Dmitry Guznenok <medsia@mail.ru>
2023-02-01 14:57:00 +0300

    Add ServiceCollectionExtensions

---
+++ LX.TestPad/DataAccess/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using LX.TestPad.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LX.TestPad.DataAccess
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEfRepositories(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<DataContext>(
+                options =>
+                {
+                    options.UseSqlServer(connectionString);
+                },
+                ServiceLifetime.Transient
+            );
+
+            services.AddScoped<Dictionary<Type, DataContext>>();
+            services.AddSingleton<DbContextFactory>();
+            services.AddSingleton<IAnswerRepository, AnswerRepository>();
+            services.AddSingleton<IQuestionRepository, QuestionRepository>();
+            services.AddSingleton<IResultAnswerRepository, ResultAnswerRepository>();
+            services.AddSingleton<IResultRepository, ResultRepository>();
+            services.AddSingleton<ITestQuestionRepository, TestQuestionRepository>();
+            services.AddSingleton<ITestRepository, TestRepository>();
+
+            return services;
+        }
+    }
+}

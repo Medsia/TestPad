f67c6acaf625288df885a0c40ce2060c9bb3bf66
Dmitry Guznenok <medsia@mail.ru>
2023-02-03 12:35:24 +0300

    Add DbContextPool instead of dbContext

--- LX.TestPad/Program.cs
+++ LX.TestPad/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
-    .Services.AddDbContext<LX.TestPad.DataAccess.DataContext>(options => options.UseSqlServer(
+    .Services.AddDbContextPool<LX.TestPad.DataAccess.DataContext>(options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEfRepositories();
 

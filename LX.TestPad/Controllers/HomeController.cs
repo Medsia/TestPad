353a2d1636f9bcbf38d84ff4c7edebe3e662c9a2
Pavel Kazutsin <pavel.kazutsin@leverx-group.com>
2023-02-06 16:58:25 +0300

    move data repositories registration to appropriate project

--- LX.TestPad/Controllers/HomeController.cs
+++ LX.TestPad/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
 ﻿using LX.TestPad.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using LX.TestPad.DataAccess;
 
 namespace LX.TestPad.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()

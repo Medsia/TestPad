71a8c7ee9e60eacd10e6036b911a7c171ad10ce4
Dmitry Guznenok <medsia@mail.ru>
2023-02-07 17:36:03 +0300

    fix entering User Name in controller

--- LX.TestPad.Business/Models/TestModel.cs
+++ LX.TestPad.Business/Models/TestModel.cs
@@ -6,5 +6,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public int TestDuration { get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+
     }
 }

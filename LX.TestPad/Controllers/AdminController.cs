fd2a8c0ac56e769261b054468771b69728c0bdf7
Dmitry Guznenok <medsia@mail.ru>
2023-02-07 15:18:45 +0300

    Add create test functionality

--- LX.TestPad/Controllers/AdminController.cs
+++ LX.TestPad/Controllers/AdminController.cs
@@ -1,45 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LX.TestPad.Business.Interfaces;
+using LX.TestPad.Business.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LX.TestPad.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ITestQuestionService _testQuestionService;
+        private readonly ITestService _testService;
+        private readonly IQuestionService _questionService;
+
+        public AdminController(ITestQuestionService testQuestionService, ITestService testService,
+            IQuestionService questionService)
+        {
+            _testQuestionService = testQuestionService;
+            _testService = testService;
+            _questionService = questionService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult TestResults()
         {
             return View();
         }
+
         public IActionResult ShowTest()
         {
             return View();
         }
+
         public IActionResult ResultDetails()
         {
             return View();
         }
+
         public IActionResult CreateTest()
         {
-            return View();
+            return View(new TestModel());
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateTest(TestModel testModel)
+        {
+            await _testService.CreateAsync(testModel);
+            return RedirectToAction(nameof(TestDetails), testModel);
+        }
+
         public IActionResult Questions()
         {
             return View();
         }
+
         public IActionResult CreateAnswer()
         {
             return View();
         }
+
         public IActionResult DeleteTest()
         {
             return View();
         }
-        public IActionResult TestDetails()
+        public IActionResult TestDetails(TestModel testModel)
         {
-            return View();
+            return View(testModel);
         }
-
     }
 }

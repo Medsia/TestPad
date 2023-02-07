8b31b43ef65eb36a32a1deebc4df21e14a9a3959
Andrei Shumilov <99248948+neerayel@users.noreply.github.com>
2023-02-07 11:52:33 +0300

    Update Questions view and controller

--- LX.TestPad.Business/Interfaces/IQuestionService.cs
+++ LX.TestPad.Business/Interfaces/IQuestionService.cs
@@ -4,6 +4,7 @@ namespace LX.TestPad.Business.Interfaces
 {
     public interface IQuestionService : IService<QuestionModel>
     {
+        Task<List<QuestionModel>> GetAllAsync();
         Task<QuestionModel> GetByIdAsync(int id);
         Task<QuestionWithAnswersModel> GetByIdIcludingAnswersAsync(int id);
     }
--- LX.TestPad/Controllers/AdminController.cs
+++ LX.TestPad/Controllers/AdminController.cs
@@ -1,45 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LX.TestPad.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LX.TestPad.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ITestService _testService;
+        private readonly IQuestionService _questionService;
+
+        public AdminController(IQuestionService questionService, ITestService testService)
+        {
+            _questionService = questionService;
+            _testService = testService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
         public IActionResult TestResults()
         {
             return View();
         }
+
+
         public IActionResult ShowTest()
         {
             return View();
         }
+
+
         public IActionResult ResultDetails()
         {
             return View();
         }
+
+
         public IActionResult CreateTest()
         {
             return View();
         }
-        public IActionResult Questions()
+
+
+        public async Task<IActionResult> Questions()
         {
+            ViewBag.Tests = await _testService.GetAllAsync();
+            ViewBag.Questions = await _questionService.GetAllAsync();
             return View();
         }
+
         public IActionResult CreateAnswer()
         {
             return View();
         }
+
+
         public IActionResult DeleteTest()
         {
             return View();
         }
+
+
         public IActionResult TestDetails()
         {
             return View();
         }
-
     }
 }

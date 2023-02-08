873d5929670073de620ac297fd59e2eac90d0a65
Dmitry Guznenok <medsia@mail.ru>
2023-02-08 16:22:36 +0300

    add "IsAnswersRelatedToOneQuestionCheck"validation and check this after resived question

--- LX.TestPad.Business/Services/ResultAnswerService.cs
+++ LX.TestPad.Business/Services/ResultAnswerService.cs
@@ -72,11 +72,11 @@ namespace LX.TestPad.Business.Services
             var answers = await _answerRepository.GetAllByIdsAsync(answersId);
             var question = await _questionRepository.GetByIdAsync(answers[0].QuestionId);
 
+            ExceptionChecker.IsAnswersRelatedToOneQuestionCheck(question.Id, answers);
+
             var items = new ResultAnswer[answers.Count];
             for(int i = 0; i < items.Length; i++) 
             {
-                if (question.Id != answers[i].QuestionId)
-                    throw new ArgumentException("Answer cannot be matched with question.", "answers[i].QuestionId");
                 items[i] = (new ResultAnswer
                 {
                     ResultId = resultId,

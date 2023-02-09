3e1f5eaed049bd35199bb7c640085614782992c3
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2023-02-09 15:00:32 +0300

    Added UI Timer

--- LX.TestPad/Controllers/TestController.cs
+++ LX.TestPad/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using LX.TestPad.Business.Interfaces;
 using LX.TestPad.Business.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace LX.TestPad.Controllers
 {
@@ -40,7 +41,8 @@ namespace LX.TestPad.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CreatePreliminaryUserResultForTest(TestModel testModel)
         {
-            var resultModel = await _resultService.CreateAsync(new ResultModel {
+            var resultModel = await _resultService.CreateAsync(new ResultModel
+            {
                 TestId = testModel.Id,
                 UserName = testModel.UserName + testModel.UserSurname,
                 Score = 0,
@@ -58,10 +60,11 @@ namespace LX.TestPad.Controllers
             if (questionNumber >= testQuestions.Count)
                 return RedirectToAction(nameof(Result));
             var question = await _questionService.GetByIdIcludingAnswersAsync(testQuestions[questionNumber].QuestionId);
+            var test = await _testService.GetByIdAsync(result.TestId);
 
             ViewBag.resultId = resultId;
             ViewBag.questionNumber = questionNumber;
-
+            ViewBag.endedAt = result.StartedAt.AddSeconds(test.TestDuration).ToString("F", new CultureInfo("en-US"));
             return View(question);
         }
 

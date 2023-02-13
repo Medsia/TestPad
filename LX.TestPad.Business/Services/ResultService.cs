2d9a01ad8113281502c3eac6cd1a7c9e7255ad3f
Andrei Shumilov <99248948+neerayel@users.noreply.github.com>
2023-02-13 13:46:21 +0300

    Fix result score calculation

--- LX.TestPad.Business/Services/ResultService.cs
+++ LX.TestPad.Business/Services/ResultService.cs
@@ -90,7 +90,9 @@ namespace LX.TestPad.Business.Services
 
                 int totalCorrectAnswersCount = (await _answerRepository.GetAllCorrectByQuestionIdAsync(testQuestion.QuestionId)).Count;
                 int correctAnswersCount = await _resultAnswerRepository.CountAllCorrectByQuestionIdAsync(resultId, testQuestion.QuestionId);
-                score += (double)correctAnswersCount / totalCorrectAnswersCount;
+
+                var questionScore = (double)correctAnswersCount / totalCorrectAnswersCount;
+                if (questionScore <= 1) score += questionScore;
             }
             score /= testQuestions.Count;
 

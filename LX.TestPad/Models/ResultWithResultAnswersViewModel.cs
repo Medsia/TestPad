307126ddb5fab15cb653f52cb2af2be3e1489e94
Andrei Shumilov <99248948+neerayel@users.noreply.github.com>
2023-02-15 17:44:54 +0300

    Add view model for passing result with it's resultAnswers in view

---
+++ LX.TestPad/Models/ResultWithResultAnswersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LX.TestPad.Business.Models;
+
+namespace LX.TestPad.Models
+{
+    public class ResultWithResultAnswersViewModel
+    {
+        public ResultIncludeTestModel resultModel { get; set; }
+        public List<IGrouping<string, ResultAnswerModel>> resultAnswerModels { get; set; }
+    }
+}

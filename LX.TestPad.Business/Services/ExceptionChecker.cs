5b78f321ee41887ef5803c99ecc9d2f2842bfb5a
Andrei Shumilov <99248948+neerayel@users.noreply.github.com>
2023-02-17 12:32:26 +0300

    Move exception messages to separate class

--- LX.TestPad.Business/Services/ExceptionChecker.cs
+++ LX.TestPad.Business/Services/ExceptionChecker.cs
@@ -9,51 +9,44 @@ namespace LX.TestPad.Business.Services
         private static int MinimalPageNumber = 1;
         private static int MinimalTestCount = 1;
 
-        private static string SqlIdMessage = "Id cannot have a value of 0 or be negative";
-        private static string PageNumberMessage = "Page number cannot be 0 or negative";
-        private static string TestPerPageCountMessage = "Quantity of tests per page cannot be 0 or negative";
-        private static string IsAnswersRelatedToOneQuestionMessage = "Answer cannot be matched with question";
-        private static string IsItemNullMessage = "Entity, received from repository, is null. Entity type: ";
-        private static string IsRequestAValidStringMessage = "Received string in null, empty or contains only whitespace characters";
-
         public static void SQLKeyIdCheck(int id)
         {
             if (id < MinimalSQLKeyId)
-                throw new ArgumentOutOfRangeException("id", id, SqlIdMessage);
+                throw new ArgumentOutOfRangeException("id", id, ExceptionMessage.SqlId);
         }
         public static void ListOfSQLKeyIdsCheck(List<int> ids)
         {
             if (ids.Any(id => id < 1))
-                throw new ArgumentOutOfRangeException("ids", ids, SqlIdMessage);
+                throw new ArgumentOutOfRangeException("ids", ids, ExceptionMessage.SqlId);
         }
 
         public static void PageNumberCheck(int pageNumber)
         {
             if (pageNumber < MinimalPageNumber)
-                throw new ArgumentOutOfRangeException("pageNumber", pageNumber, PageNumberMessage);
+                throw new ArgumentOutOfRangeException("pageNumber", pageNumber, ExceptionMessage.PageNumber);
         }
         public static void TestPerPageCountCheck(int count)
         {
             if (count < MinimalTestCount)
-                throw new ArgumentOutOfRangeException("count", count, TestPerPageCountMessage);
+                throw new ArgumentOutOfRangeException("count", count, ExceptionMessage.TestPerPageCount);
         }
         public static void IsAnswersRelatedToOneQuestionCheck(int questionId, List<Answer> answers)
         {
             foreach (var answer in answers)
             {
                 if (questionId != answer.QuestionId)
-                    throw new ArgumentException(IsAnswersRelatedToOneQuestionMessage, "answers[i].QuestionId");
+                    throw new ArgumentException(ExceptionMessage.IsAnswersRelatedToOneQuestion, "answers[i].QuestionId");
             }
         }
         public static void IsItemNullCheck<T>(T entity)
         {
             if (entity == null)
-                throw new ArgumentNullException("entity", IsItemNullMessage + entity.GetType().ToString());
+                throw new ArgumentNullException("entity", ExceptionMessage.IsItemNull + entity.GetType().ToString());
         }
         public static void IsRequestAValidString(string request)
         {
             if (string.IsNullOrWhiteSpace(request))
-                throw new ArgumentException(IsRequestAValidStringMessage, "request");
+                throw new ArgumentException(ExceptionMessage.IsRequestAValidString, "request");
         }
     }
 }
---
+++ LX.TestPad.Business/Services/ExceptionMessage.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LX.TestPad.Business.Services
+{
+    public static class ExceptionMessage
+    {
+        public static string SqlId { get; } = "Id cannot have a value of 0 or be negative";
+        public static string PageNumber { get; } = "Page number cannot be 0 or negative";
+        public static string TestPerPageCount { get; } = "Quantity of tests per page cannot be 0 or negative";
+        public static string IsAnswersRelatedToOneQuestion { get; } = "Answer cannot be matched with question";
+        public static string IsItemNull { get; } = "Entity, received from repository, is null. Entity type: ";
+        public static string IsRequestAValidString { get; } = "Received string in null, empty or contains only whitespace characters";
+    }
+}

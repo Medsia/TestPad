c6c382b18aceac30674ed4a6a77e263f52b6d3a5
Pavel Kazutsin <pavel.kazutsin@leverx-group.com>
2023-03-06 14:17:17 +0300

    add email field

--- LX.TestPad.DataAccess/Entities/Result.cs
+++ LX.TestPad.DataAccess/Entities/Result.cs
@@ -9,6 +9,7 @@ namespace LX.TestPad.DataAccess.Entities
         public int Id { get; set; }
         public int TestId { get; set; }
         public string UserName { get; set; }
+        public string? Email { get; set; }
         public double Score { get; set; }
         public bool IsCalculated { get; set; }
         public DateTime StartedAt { get; set; }

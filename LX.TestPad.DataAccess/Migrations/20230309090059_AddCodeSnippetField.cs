8487f0a8d340422a16e60950be9a12c97719e61b
Pavel Kazutsin <pavel.kazutsin@leverx-group.com>
2023-03-09 12:44:37 +0300

    add migrations

---
+++ LX.TestPad.DataAccess/Migrations/20230309090059_AddCodeSnippetField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LX.TestPad.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeSnippetField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CodeSnippet",
+                table: "Questions",
+                type: "nvarchar(2048)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodeSnippet",
+                table: "Questions");
+        }
+    }
+}
